7f334d2764c9a801d9ad17026ff6e4473e9670be
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2024-01-12 14:49:28 +0900

    [level Lv.0] Title: 수 조작하기 1, Time: 1.82 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,11 @@
+def solution(n, control):
+    for i in control : 
+        if i == 'w' : 
+            n+=1
+        elif i == 's' : 
+            n-=1
+        elif i == 'd' : 
+            n+=10
+        else : 
+            n -=10
+    return n 
